ffcb9469aace8ee146a636c4ddbcb3cbe228ed22
Brant Keener <brant_keener@yahoo.com>
2019-01-15 14:23:55 -0700

    Add supervisor department name add functionality

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,8 +1,4 @@
 
-// TODO Supervisor options: View Product Sales by Department, Create a New Department
-// TODO total_profit column is updated in app, and is prodcut_sales - over_head_costs. Do not store this value in DB
-// TODO Check manager TODO
-// TODO Check customer TODO
 
 const env = require('dotenv').config();
 const mysql = require('mysql');
@@ -26,12 +22,14 @@ sqlDBConnection.connect((err) => {
 (superMenu = (itterations) => {
     if(itterations === undefined) {
         console.log(`\nYou are logged in as Supervisor. Please select an item to continue.`);
+    } else {
+        console.log(`\nWhat would you like to do next?`);
     };
     inquirer.prompt([
         {
             type: 'list',
             message: '\nWhat would you like to do today?',
-            choices: ['View Product Sales by Department', 'Create a New Department'],
+            choices: ['View Product Sales by Department', 'Create a New Department', 'Exit'],
             name: 'superChoice'
         }
     ]).then(res => {
@@ -42,10 +40,77 @@ sqlDBConnection.connect((err) => {
             FROM departments INNER JOIN products ON departments.department_name = products.department_name
             GROUP BY departments.department_name;`, (err, res) => {
                 if(err) throw err;
+                console.log(`\nDepartment Cost, Sales, and Profit\n`)
                 console.log(table.print(res));
+                superMenu(1);
             });
         } else if(res.superChoice === 'Create a New Department') {
-
+            createDepartment();
+        } else {
+            exitApp();
         };
     });
 })();
+
+
+// Keep working on this
+const createDepartment = () => {
+    let departmentArray = [];
+    let query = `SELECT department_name FROM departments;`
+    sqlDBConnection.query(query, (err, res) => {
+        if(err) throw err;
+        res.forEach(element => {
+            let lowerElem = (element.department_name).toLowerCase();
+            let noSpaceElem = lowerElem.replace(/\W/g, '');
+            departmentArray.push(noSpaceElem);
+        });
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name you would like to assign to the new department?',
+                validate: (answer) => {
+                    let lowerA = answer.toLowerCase();
+                    let noSpaceA = lowerA.replace(/\W/g, '');
+                    for(let i = 0; i < departmentArray.length; i++) {
+                        if(departmentArray[i] === noSpaceA) {
+                            return `That name, or something very similar is already in use. Please choose something more unique.`;
+                        }
+                    };
+                    return true;
+                },
+                name: `newDepartmentName`
+            },
+            {
+                type: 'input',
+                message: 'What is the over head cost for this department? (format: xxxx where the last 2 digits are the cents value)',
+                validate: (res) => {
+                    let pass = res.match(/[0-9]/g);
+                    let fail = res.match(/['a-zA-Z']/g);
+                    if(pass && !fail) {
+                        return true;
+                    }
+                    return `Make sure your entry is a number in the correct format. (xxxx last 2 digits are cents value)`
+                },
+                name: 'newDepartmentOverhead'
+            }
+        ]).then(res => {
+            let newName = res.newDepartmentName;
+            let overhead = res.newDepartmentOverhead;
+            let tempArray = [...overhead];
+            tempArray.splice(tempArray.length - 2, 0, '.');
+            let newOverhead = parseFloat(tempArray.join(''));
+            let query = `INSERT INTO departments (department_name, over_head_costs) 
+            VALUES ('${newName}', ${newOverhead})`
+            sqlDBConnection.query(query, (err, res) => {
+                if(err) throw err;
+                console.log(`\nDepartment ${newName} has been added with an Overhead cost of $${newOverhead}`);
+                superMenu(1);
+            });
+        });
+    });
+};
+
+const exitApp = () => {
+    console.log(`\nYou have logged out of Supervisor\n`)
+    sqlDBConnection.end();
+};
